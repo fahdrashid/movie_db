cca7d34ecca1785b2dd1ba77d0e41e1a2fd80cef
Fahad Rashid <fahd.rashid96@gmail.com>
2024-10-25 09:44:31 -0500

    uploads

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,16 @@ import { UsersModule } from './users/users.module';
 import { Movie } from './movies/movie.entity';
 import { User } from './users/user.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),  // Set the directory path
+      serveRoot: '/uploads',  // URL prefix to access the files
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule, Movie, User],
