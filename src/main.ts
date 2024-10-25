2a1a5a4141d0937c74cd14c00459e855607001d5
Fahad Rashid <fahd.rashid96@gmail.com>
2024-10-25 05:33:18 -0500

    initial

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // Swagger configuration
   const config = new DocumentBuilder()
@@ -22,7 +23,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
   await app.listen(3000);
 }
 bootstrap();
