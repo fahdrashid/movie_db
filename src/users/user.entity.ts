1fd92e3ccb4b15035b53837818f210f20b4d2967
Fahad Rashid <fahd.rashid96@gmail.com>
2024-10-24 22:15:55 -0500

    initial

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Movie } from 'src/movies/movie.entity';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +11,7 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Movie, (movie) => movie.user)
+  movies: Movie[];
 }
